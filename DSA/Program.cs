7860ff77b00214df4bfbfcc84275bbcb5f526e6e
Sagar <khatrisagar415@gmail.com>
2026-02-07 00:22:13 +0530

    Finding the largest value from the list

---
+++ DSA/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DSA;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> nums = [12, 23, 45, 4];
+        var largest = FindLargest(nums);
+        Console.WriteLine(largest);
+    }
+
+    private static int FindLargest(List<int> nums)
+    {
+        var largest = int.MinValue;
+        foreach (var num in nums) largest = Math.Max(largest, num);
+
+        return largest;
+    }
+}
