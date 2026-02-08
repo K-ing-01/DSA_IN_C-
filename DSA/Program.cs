ac963a54a0266be31d0b563a2696644f95eea3f9
Sagar <khatrisagar415@gmail.com>
2026-02-08 19:23:10 +0530

    Added kaden`s algorithm

--- DSA/Program.cs
+++ DSA/Program.cs
@@ -4,12 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        List<int> nums = [21, 31, 44, 45, 51, 56, 78, 99];
-        var key = 51;
-        // var largest = FindLargest(nums);
-        // Console.WriteLine(largest);
-        var index = BinarySearch(nums, key);
-        Console.WriteLine(index);
+        // List<int> nums = [21, 31, 44, 45, 51, 56, 78, 99];
+        // var key = 51;
+        // // var largest = FindLargest(nums);
+        // // Console.WriteLine(largest);
+        // var index = BinarySearch(nums, key);
+        List<int> nums = [-1, -2, 4, -1, -2, 1, 5, -3];
+        var maxSum = MaximumSubArraySum(nums);
+        Console.WriteLine(maxSum);
     }
 
     private static int FindLargest(List<int> nums)
@@ -39,5 +41,19 @@ internal class Program
         }
 
         return -1;
+    }
+
+    public static int MaximumSubArraySum(List<int> nums)
+    {
+        int currentSum = 0, maxSum = int.MinValue;
+        for (var i = 0; i < nums.Count(); i++)
+        {
+            if (currentSum < 0) currentSum = 0;
+
+            currentSum = currentSum + nums[i];
+            maxSum = Math.Max(currentSum, maxSum);
+        }
+
+        return maxSum;
     }
 }
